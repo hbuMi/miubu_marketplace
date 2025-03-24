a9b57e2d19c33a95edbb15c978aab7922204e806
jbuMu <jussi.tiimalasi@gmail.com>
2025-03-24 13:35:30 +0200

    Show item list/info

--- items.py
+++ items.py
@@ -3,3 +3,11 @@ import db
 def add_item(title, descr, price, user_id):
     sql = """INSERT INTO items (title, descr, price, user_id) VALUES (?, ?, ?, ?)"""
     db.execute(sql, [title, descr, price, user_id])
+
+def get_items():
+    sql = "SELECT id, title FROM items ORDER BY id DESC"
+    return db.query(sql)
+
+def get_items(item_id):
+    sql = """SELECT items.title, items.descr, items.price, users.username FROM items, users WHERE items.user_id = users.id AND items.id = ?"""
+    return db.query(sql, [item_id])[0]
