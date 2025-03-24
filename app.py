0b33bfaf33842bc63f878d6842baf66094ec4090
jbuMu <jussi.tiimalasi@gmail.com>
2025-03-24 12:13:27 +0200

    Add new item form

--- app.py
+++ app.py
@@ -41,17 +41,37 @@ def login():
     if request.method == "POST":
         username = request.form["username"]
         password = request.form["password"]
-    
-    sql = "SELECT password_hash FROM users WHERE username = ?"
-    password_hash = db.query(sql, [username])[0][0]
+
+    sql = "SELECT id, password_hash FROM users WHERE username = ?"
+    result = db.query(sql, [username])[0]
+    user_id = result["id"]
+    password_hash = result["password_hash"]
 
     if check_password_hash(password_hash, password):
+        session["user_id"] = user_id
         session["username"] = username
         return redirect("/")
     else:
         return "VIRHE: väärä tunnus tai salasana"
 
+@app.route("/new_item")
+def new_item():
+    return render_template("new_item.html")
+
+@app.route("/create_item", methods=["POST"])
+def create_item():
+    title = request.form["title"]
+    descr = request.form["descr"]
+    price = request.form["price"]
+    user_id = session["user_id"]
+
+    sql = """INSERT INTO items (title, descr, price, user_id) VALUES (?, ?, ?, ?)"""
+    db.execute(sql, [title, descr, price, user_id])
+
+    return redirect("/")
+
 @app.route("/logout")
 def logout():
+    del session["user_id"]
     del session["username"]
     return redirect("/")
